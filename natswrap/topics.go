516a8b82dda2ec6cfc849cbb85d58dbd3cb32fd9
Michael Popsuev <michaelinn4sci@gmail.com>
2018-03-28 13:08:29 +0300

    Add topics const and message struct

---
+++ natswrap/topics.go
@@ -0,0 +1,19 @@
+package natswrap
+
+type Message struct {
+	EventID string      `json:"eventId"`
+	Result  string      `json:"result"`
+	Msg     string      `json:"msg"`
+	Details interface{} `json:"details"`
+}
+
+const (
+	TopicTxDepositNew     = "tx.deposit.new"
+	TopicTxDepositResults = "tx.deposit.results"
+
+	TopicTxPaymentNew     = "tx.payment.new"
+	TopicTxPaymentResults = "tx.payment.results"
+
+	TopicTxCommissionClearingNew     = "tx.commission_clearing.new"
+	TopicTxCommissionClearingResults = "tx.commission_clearing.results"
+)
