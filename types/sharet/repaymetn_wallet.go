289f5a82e1a65fdafa14df0415dc75246ccf5c43
Valeriy.Gello <valeriy.gello@inn4science.com>
2018-04-20 17:57:44 +0300

    fix json field name

--- types/sharet/repaymetn_wallet.go
+++ types/sharet/repaymetn_wallet.go
@@ -3,7 +3,7 @@ package sharet
 import "gitlab.inn4science.com/vcg/go-common/types/currency"
 
 type RepaymentWallet struct {
-	WalletId    string `db:"wallet_id"`
+	WalletId    string `db:"walletId"`
 	SharesCount int64  `db:"count"`
 	Amount      currency.Coin
 }
