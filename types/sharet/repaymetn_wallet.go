200bdf87b4be26e1b6c6d8511b8d46d2d8c8d8d1
Valeriy.Gello <valeriy.gello@inn4science.com>
2018-04-23 13:15:16 +0300

    fix-up

--- types/sharet/repaymetn_wallet.go
+++ types/sharet/repaymetn_wallet.go
@@ -3,7 +3,7 @@ package sharet
 import "gitlab.inn4science.com/vcg/go-common/types/currency"
 
 type RepaymentWallet struct {
-	WalletId    string `db:"walletId"`
-	SharesCount int64  `db:"count"`
-	Amount      currency.Coin
+	WalletId    string        `db:"walletId" json:"walletId"`
+	SharesCount int64         `db:"count" json:"sharesCount"`
+	Amount      currency.Coin `db:"amount" json:"amount"`
 }
