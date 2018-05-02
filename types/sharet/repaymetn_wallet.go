2a52c531c6bc69515406beab56c3a33d31cc6cb0
Valeriy.Gello <valeriy.gello@inn4science.com>
2018-05-02 14:42:02 +0300

    Update repayment wallet structure

--- types/sharet/repaymetn_wallet.go
+++ types/sharet/repaymetn_wallet.go
@@ -1,6 +1,7 @@
 package sharet
 
 type RepaymentWallet struct {
-	WalletId    string `db:"walletId" json:"walletId"`
+	WalletId    string `db:"wallet_id" json:"walletId"`
+	UserId      int64  `db:"user_id" json:"userId"`
 	SharesCount int64  `db:"count" json:"sharesCount"`
 }
