29588ae732af13e7a46daaf23ccdd52be38d8699
Michael Popsuev <michaelinn4sci@gmail.com>
2018-05-14 14:17:11 +0300

    Add new default results, small code reorganization

--- api/render/main.go
+++ api/render/main.go
@@ -1,101 +1,50 @@
 package render
 
-import "net/http"
-
-// R is a default struct for json responses.
-
-//Example:
-//
-//``` go
-//	func MyHandler(w http.ResponseWriter, r *http.Request) {
-//		// some code ...
-//		// ...
-//		res := render.R{
-//			Code: http.StatusOk,
-//			Message: "User created",
-//		}
-//		res.Render(w)
-//		return
-//	}
-//```
-// Usage of predefined response:
-//``` go
-//	func MyHandler(w http.ResponseWriter, r *http.Request) {
-//		// some code ...
-//		// ...
-//		render.ResultBadRequest.SetError("Invalid email").Render(w)
-//		return
-//	}
-//```
-type R struct {
-	Code    int         `json:"errcode"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   interface{} `json:"errmsg,omitempty"`
-}
-
-var (
-	// ResultServerError predefined response for `http.StatusInternalServerError`.
-	ResultServerError = &R{
-		Code:    http.StatusInternalServerError,
-		Message: "Request Failed",
-	}
-
-	// ResultBadRequest predefined response for `http.StatusBadRequest`.
-	ResultBadRequest = &R{
-		Code:    http.StatusBadRequest,
-		Message: "Bad Request",
-	}
-
-	// ResultSuccess predefined response for `http.StatusOK`.
-	ResultSuccess = &R{
-		Code:    http.StatusOK,
-		Message: "Ok",
-	}
-
-	// ResultNotFound predefined response for `http.StatusNotFound`.
-	ResultNotFound = &R{
-		Code:    http.StatusNotFound,
-		Message: "Not Found",
-	}
-
-	// ResultUnauthorized predefined response for `http.StatusUnauthorized`.
-	ResultUnauthorized = &R{
-		Code:    http.StatusUnauthorized,
-		Message: "Action Unauthorized",
-	}
+import (
+	"encoding/json"
+	"net/http"
 )
 
-// SetError adds error details to response.
-func (r *R) SetError(val interface{}) *R {
-	nr := *r
+// PrettyMarshal is a flag that enable marshalling with indent
+var PrettyMarshal bool
 
-	switch val.(type) {
-	case nil:
-		break
-	case error:
-		nr.Error = val.(error).Error()
-	case string:
-		nr.Error = val
-	case R:
-		nr.Error = val.(R).Error
-	case *R:
-		nr.Error = val.(*R).Error
-	default:
-		nr.Error = val
+// WriteJSON writes some response as WriteJSON to the `http.ResponseWriter`.
+func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
+	var marshaled []byte
+	var err error
+
+	if PrettyMarshal {
+		marshaled, err = json.MarshalIndent(data, "", "  ")
+	} else {
+		marshaled, err = json.Marshal(data)
 	}
 
-	return &nr
+	if err != nil {
+		http.Error(w, "error while render response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	w.Write(marshaled)
 }
 
-// SetData sets response data.
-func (r *R) SetData(val interface{}) *R {
-	nr := *r
-	nr.Data = val
-	return &nr
+// ServerError renders default http.StatusInternalServerError.
+func ServerError(w http.ResponseWriter) {
+	WriteJSON(w, http.StatusInternalServerError, ResultServerError)
 }
 
-// Render writes current response as WriteJSON to the `http.ResponseWriter`.
-func (r *R) Render(w http.ResponseWriter) {
-	WriteJSON(w, r.Code, r)
+// Success renders `result` as JSON with `http.StatusOK`.
+func Success(w http.ResponseWriter, result interface{}) {
+	WriteJSON(w, http.StatusOK, result)
+}
+
+// BadRequest renders `ResultBadRequest` with `reason` as an error.
+func BadRequest(w http.ResponseWriter, reason interface{}) {
+	ResultBadRequest.SetError(reason).Render(w)
+}
+
+// Unauthorized renders `ResultUnauthorized` with `reason` as an error.
+func Unauthorized(w http.ResponseWriter, reason interface{}) {
+	ResultUnauthorized.SetError(reason).Render(w)
 }
--- api/render/result.go
+++ api/render/result.go
@@ -1,45 +1,71 @@
 package render
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-// PrettyMarshal is a flag that enable marshalling with indent
-var PrettyMarshal bool
+// R is a default struct for json responses.
 
-// WriteJSON writes some response as WriteJSON to the `http.ResponseWriter`.
-func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
-	var marshaled []byte
-	var err error
+//Example:
+//
+//``` go
+//	func MyHandler(w http.ResponseWriter, r *http.Request) {
+//		// some code ...
+//		// ...
+//		res := render.R{
+//			Code: http.StatusOk,
+//			Message: "User created",
+//		}
+//		res.Render(w)
+//		return
+//	}
+//```
+// Usage of predefined response:
+//``` go
+//	func MyHandler(w http.ResponseWriter, r *http.Request) {
+//		// some code ...
+//		// ...
+//		render.ResultBadRequest.SetError("Invalid email").Render(w)
+//		return
+//	}
+//```
+type R struct {
+	Code    int         `json:"errcode"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   interface{} `json:"errmsg,omitempty"`
+}
 
-	if PrettyMarshal {
-		marshaled, err = json.MarshalIndent(data, "", "  ")
-	} else {
-		marshaled, err = json.Marshal(data)
+// SetError adds error details to response.
+func (r *R) SetError(val interface{}) *R {
+	nr := *r
+
+	switch val.(type) {
+	case nil:
+		break
+	case error:
+		nr.Error = val.(error).Error()
+	case string:
+		nr.Error = val
+	case R:
+		nr.Error = val.(R).Error
+	case *R:
+		nr.Error = val.(*R).Error
+	default:
+		nr.Error = val
 	}
 
-	if err != nil {
-		http.Error(w, "error while render response", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(status)
-	w.Write(marshaled)
+	return &nr
 }
 
-// ServerError renders default http.StatusInternalServerError.
-func ServerError(w http.ResponseWriter) {
-	WriteJSON(w, http.StatusInternalServerError, ResultServerError)
+// SetData sets response data.
+func (r *R) SetData(val interface{}) *R {
+	nr := *r
+	nr.Data = val
+	return &nr
 }
 
-// BadRequest renders `ResultBadRequest` with `reason` as an error.
-func BadRequest(w http.ResponseWriter, reason interface{}) {
-	ResultBadRequest.SetError(reason).Render(w)
-}
-
-// Success renders `result` as JSON with `http.StatusOK`.
-func Success(w http.ResponseWriter, result interface{}) {
-	WriteJSON(w, http.StatusOK, result)
+// Render writes current response as WriteJSON to the `http.ResponseWriter`.
+func (r *R) Render(w http.ResponseWriter) {
+	WriteJSON(w, r.Code, r)
 }
