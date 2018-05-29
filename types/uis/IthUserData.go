b4f9bd5e2fe7306011b778803d2d647e77fe261a
Eugene Babich <eugene.babich@inn4science.com>
2018-05-29 19:17:23 +0300

    ext.user model in user integration service

--- types/uis/IthUserData.go
+++ types/uis/IthUserData.go
@@ -1,20 +1,25 @@
 package uis
 
-type IthUserData struct {
-	Id        int64  `json:"id"`
-	AccountId string `json:"accountId" db:"account_id"`
-	AccessToken string `json:"accessToken" db:"access_token"`
-	AccountType string `json:"accountType" db:"account_type"`
-	AffilateId string `json:"affilateId" db:"affilate_id"`
-	CampaignId string `json:"campaignId" db:"campaign_id"`
-	BannerId string `json:"bannerId" db:"banner_id"`
-	CustomParameters string `json:"customParameters" db:"custom_parameters"`
-	AccountSecret string `json:"accountSecret" db:"account_secret"`
-	MerchantUid string `json:"merchantUid" db:"merchant_uid"`
-	AccountTimezone int `json:"accountTimezone" db:"account_timezone"`
-	AccountWeekStartsOn string `json:"accountWeekStartsOn" db:"account_week_starts_on"`
-	CurrencyConversion bool `json:"currencyConversion" db:"currency_conversion"`
-	AlwaysRefundEwallet bool `json:"alwaysRefundEwallet" db:"always_refund_ewallet"`
-	ConfirmOutTransaction bool
+import "gitlab.inn4science.com/vcg/go-common/ith/ams"
 
+type IthUserData struct {
+	Id                        int64                  `json:"id"`
+	AccountId                 string                 `json:"accountId" db:"account_id"`
+	AccessToken               string                 `json:"accessToken" db:"access_token"`
+	AccountType               ams.AccountType        `json:"accountType" db:"account_type"`
+	AccountStatus             ams.AccountStatus      `json:"accountStatus" db:"account_status"`
+	AffilateId                string                 `json:"affilateId" db:"affilate_id"`
+	CampaignId                string                 `json:"campaignId" db:"campaign_id"`
+	BannerId                  string                 `json:"bannerId" db:"banner_id"`
+	CustomParameters          string                 `json:"customParameters" db:"custom_parameters"`
+	AccountSecret             string                 `json:"accountSecret" db:"account_secret"`
+	MerchantUid               string                 `json:"merchantUid" db:"merchant_uid"`
+	AccountTimezone           int                    `json:"accountTimezone" db:"account_timezone"`
+	AccountWeekStartsOn       string                 `json:"accountWeekStartsOn" db:"account_week_starts_on"`
+	CurrencyConversion        bool                   `json:"currencyConversion" db:"currency_conversion"`
+	AlwaysRefundEwallet       bool                   `json:"alwaysRefundEwallet" db:"always_refund_ewallet"`
+	ConfirmOutTransaction     bool                   `json:"confirmOutTransaction" db:"confirm_out_transaction"`
+	ConfirmLogin              bool                   `json:"confirmLogin" db:"confirm_login"`
+	ActionConfirmationEnabled bool                   `json:"actionConfirmationEnabled" db:"action_confirmation_enabled"`
+	ActionConfirmationType    ams.ActionConfirmation `json:"actionConfirmationType" db:"action_confirmation_type"`
 }
--- types/uis/user.go
+++ types/uis/user.go
@@ -6,25 +6,28 @@ import (
 	"fmt"
 )
 
-type User struct {
-	Id                int64         `json:"id" db:"id"`
-	Phone             string        `json:"phone" db:"phone"`
-	Email             string        `json:"email" db:"email"`
-	Status            UserStatus    `json:"status" db:"status"`
-	FirstName         string        `json:"firstName" db:"first_name"`
-	LastName          string        `json:"lastName" db:"last_name"`
-	BirthDate         UserBirthDate `json:"birthDate" db:"birth_date"`
-	LanguageMarker    string        `json:"languageMarker" db:"language_marker"`
-	CountryMarker     string        `json:"countryMarker" db:"country_marker"`
-	PreferredCurrency string        `json:"preferredCurrency" db:"preferred_currency"`
-	MailVerified      bool          `json:"mailVerified" db:"mail_verified"`
-	UserKey           string        `json:"-" db:"user_key"`
-	CreatedAt         int64         `json:"createdAt" db:"created_at"`
-	UpdatedAt         int64         `json:"updatedAt" db:"updated_at"`
-}
-
-type UserStatus int
-type UserBirthDate int64
+type (
+	User struct {
+		Id                int64          `json:"id" db:"id"`
+		Phone             string         `json:"phone" db:"phone"`
+		Email             string         `json:"email" db:"email"`
+		Status            UserStatus     `json:"status" db:"status"`
+		FirstName         string         `json:"firstName" db:"first_name"`
+		LastName          string         `json:"lastName" db:"last_name"`
+		BirthDate         UserBirthDate  `json:"birthDate" db:"birth_date"`
+		LanguageMarker    string         `json:"languageMarker" db:"language_marker"`
+		CountryMarker     string         `json:"countryMarker" db:"country_marker"`
+		PreferredCurrency string         `json:"preferredCurrency" db:"preferred_currency"`
+		MailVerified      bool           `json:"mailVerified" db:"mail_verified"`
+		UserKey           string         `json:"-" db:"user_key"`
+		CreatedAt         int64          `json:"createdAt" db:"created_at"`
+		UpdatedAt         int64          `json:"updatedAt" db:"updated_at"`
+		Integrations      IntegrationMap `json:"integrations,omitempty" db:"-"`
+	}
+	IntegrationMap map[string]interface{}
+	UserStatus int
+	UserBirthDate int64
+)
 
 const (
 	UserStatusBlocked        UserStatus = -10
