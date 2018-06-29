6ba26f45a23265d4c48b5e2c5ce6cefd97511630
Michael Popsuev <michael.popsuev@inn4science.com>
2018-06-29 20:14:35 +0300

    Update methods

--- api/httpx/main.go
+++ api/httpx/main.go
@@ -1,15 +1,67 @@
 package httpx
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
 
 	"github.com/pkg/errors"
 )
 
-var httpClient = http.DefaultClient
+type Headers map[string]string
+
+const defaultTimeout = time.Second * 15
+
+var httpClient = http.Client{Timeout: defaultTimeout}
+
+// SetTimeout updated `httpClient` default timeout (15s).
+func SetTimeout(duration time.Duration) {
+	httpClient.Timeout = duration
+}
+
+// PostJSON, sets passed `headers` and `body` and executes RequestJSON with POST method.
+func PostJSON(url string, body interface{}, headers map[string]string) (*http.Response, error) {
+	return RequestJSON(http.MethodPost, url, body, headers)
+}
+
+// PutJSON, sets passed `headers` and `body` and executes RequestJSON with PUT method.
+func PutJSON(url string, body interface{}, headers map[string]string) (*http.Response, error) {
+	return RequestJSON(http.MethodPut, url, body, headers)
+}
+
+// GetJSON, sets passed `headers` and executes RequestJSON with GET method.
+func GetJSON(url string, headers map[string]string) (*http.Response, error) {
+	return RequestJSON(http.MethodGet, url, nil, headers)
+}
+
+// DeleteJSON, sets passed `headers` and executes RequestJSON with DELETE method.
+func DeleteJSON(url string, headers map[string]string) (*http.Response, error) {
+	return RequestJSON(http.MethodDelete, url, nil, headers)
+}
+
+// RequestJSON creates and executes new request with JSON content type.
+func RequestJSON(method string, url string, data interface{}, headers map[string]string) (*http.Response, error) {
+	var body io.Reader = nil
+
+	if data != nil {
+		rawData, err := json.Marshal(data)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to marshal body")
+		}
+		body = bytes.NewBuffer(rawData)
+	}
+
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Set("Content-Type", "application/json")
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	return httpClient.Do(req)
+}
 
 func ParseJSONBody(r *http.Request, dest interface{}) error {
 	defer r.Body.Close()
@@ -27,32 +79,4 @@ func ParseJSONResult(httpResp *http.Response, dest interface{}) error {
 		return errors.Wrap(err, "failed to unmarshal response body")
 	}
 	return nil
-}
-
-func PostJSON(url string, data interface{}, headers map[string]string) (*http.Response, error) {
-	return requestJSON("POST", url, data, headers)
-}
-
-func PutJSON(url string, data interface{}, headers map[string]string) (*http.Response, error) {
-	return requestJSON("PUT", url, data, headers)
-}
-
-func DeleteJSON(url string, data interface{}, headers map[string]string) (*http.Response, error) {
-	return requestJSON("DELETE", url, data, headers)
-}
-
-func requestJSON(method string, url string, data interface{}, headers map[string]string) (*http.Response, error) {
-	rawData, err := json.Marshal(data)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to marshal body")
-	}
-
-	req, _ := http.NewRequest(method, url, bytes.NewBuffer(rawData))
-	req.Header.Set("Content-Type", "application/json")
-
-	for key, value := range headers {
-		req.Header.Set(key, value)
-	}
-
-	return httpClient.Do(req)
 }
